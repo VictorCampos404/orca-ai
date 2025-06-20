fec0227b17e35ea496c64f6b456d3b4b468bfb11
Victor Campos <campos.victor@dilettasolutions.com>
2025-06-20 18:57:50 -0300

    Conectando firebase

--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -5,12 +6,17 @@ import 'package:orca_ai/app_module.dart';
 import 'package:orca_ai/core/configs/api_config.dart';
 import 'package:orca_ai/core/constants/routes.dart';
 import 'package:orca_ai/core/utils/remote_config.dart';
+import 'package:orca_ai/firebase_options.dart';
 
 class AppSettings {
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
 
     initializeDateFormatting('pt_BR');
+
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
     Modular.init(AppModule());
 
